879e4e82583446595f8e80d75192db283b63b19d
Nesrine <nesrinakon@gmail.com>
2017-11-06 09:51:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function takeANumber(line, person){
   line.push(person)
   return (`Welcome, ${person}. You are number ${line.indexOf(person)+1} in line.`)
 }
+function nowServing(line){
+  if(line.length==0){
+    return ("There is nobody waiting to be served!")
+  }
+  else{
+    var person = line[0];
+    line[0].shift()
+    return (`Currently serving ${person}.`)
+  }
+}
