f947bb44f522a1bbf67e88b2ee820c8ae88e995e
Nesrine <nesrinakon@gmail.com>
2017-11-06 09:55:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ function nowServing(line){
     return (`Currently serving ${person}.`)
   }
 }
+function currentLine(line){
+  if(line.length==0){
+    return ("The line is currently empty.")
+  }
+  else{
+    lineString = "The line is currently: "
+    for(let i=0; i<line.length; i++){
+      lineString +=
+    }
+  }
+}
